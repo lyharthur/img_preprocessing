f4b378beb936e83ad5b59af282d6165eebaf645f
lyharthur <lyharthur@gmail.com>
2016-07-11 17:35:25 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,28 +4,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import cv2
-rowN = 300
-colN = 240
-img1 = cv2.imread("d8_P4PB25#03_DEF.tif")
-img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-img1 = img1[0:240,0:240]
+
+img_orig = cv2.imread("d8_P4PB25#03_REF.tif")
+img_orig = cv2.cvtColor(img_orig, cv2.COLOR_BGR2GRAY)
+img1 = img_orig[0:240,0:240]
 img1 = cv2.GaussianBlur(img1,(5,5),0)
+img1 = cv2.Canny(img1,100,200)
+
 cv2.imwrite('12.jpg',img1)
 
-img2 = cv2.imread("d8_P4PB25#03_REF.tif")
+img2 = cv2.imread("d8_P4PB25#03_DEF.tif")
 img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-img2 = img2[0:150,0:150]
-template = cv2.GaussianBlur(img2,(5,5),0)
+template = img2[50:150,50:150]
+template = cv2.GaussianBlur(template,(5,5),0)
+template = cv2.Canny(template,100,200)
+
 cv2.imwrite('22.jpg',img2)
 
 w, h = template.shape[::-1]
 
 res = cv2.matchTemplate(img1,template,cv2.TM_CCOEFF_NORMED)
 min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-
-# If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-
+print(min_val, max_val, min_loc, max_loc)
 
 top_left = max_loc
 bottom_right = (top_left[0] + w, top_left[1] + h)
@@ -39,10 +39,7 @@ plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
 plt.suptitle('cv2.TM_CCOEFF_NORMED')
 plt.show()
 
-
-
-
-
-
-
+img3 = img_orig[(50 - top_left[0]):,(50 - top_left[1]):]
+img3 = img2 - img_orig
+cv2.imwrite('23.jpg',img3)
 print("done")
